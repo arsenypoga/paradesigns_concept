8f5a8ac5c100736f19983537066286eeee215f9d
ArsenyPoga <semiretoja@gmail.com>
2016-05-22 23:00:18 -0400

    few corrections to an index

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,18 +11,18 @@ $(document).ready(function() {
         loopTop: true,
         verticalAllign: false,
         onLeave: function(index, nextIndex, direction) {
-          if ((index == 1 && direction == 'down' && nextIndex == 2 )|| (index == 3 && direction == 'up' && nextIndex == 2)){
-            $('#Wallker').show('slow');
-          }
-           if((index ==1 && direction == 'down' && nextIndex == 3) || (index == 2 && direction == 'down' && nextIndex == 3)){
-            $('#Maestro').show('slow');
-          }
-          if(index == 2 && (direction == 'up' || direction == 'down')){
-            $('#Wallker').hide('slow');
-          }
-          if(index == 3 && direction == 'up' && (nextIndex == 1 || nextIndex == 2)){
-            $('#Maestro').hide('slow');
-          }
+            if ((index == 1 && direction == 'down' && nextIndex == 2) || (index == 3 && direction == 'up' && nextIndex == 2)) {
+                $('#Wallker').show('slow');
+            }
+            if ((index == 1 && direction == 'down' && nextIndex == 3) || (index == 2 && direction == 'down' && nextIndex == 3)) {
+                $('#Maestro').show('slow');
+            }
+            if (index == 2 && (direction == 'up' || direction == 'down')) {
+                $('#Wallker').hide('slow');
+            }
+            if (index == 3 && direction == 'up' && (nextIndex == 1 || nextIndex == 2)) {
+                $('#Maestro').hide('slow');
+            }
         }
 
     });
@@ -30,34 +30,5 @@ $(document).ready(function() {
     $('#arrow').click(function(event) {
         console.log("clicked");
         $.fn.pagepiling.moveSectionDown();
-
-    $('#Wallker').hide();
-    $('#Maestro').hide();
-    $('#pagepiling').pagepiling({
-        loopBottom: true,
-        loopTop: true,
-        verticalAllign: false,
-        onLeave: function(index, nextIndex, direction) {
-          if ((index == 1 && direction == 'down' && nextIndex == 2 )|| (index == 3 && direction == 'up' && nextIndex == 2)){
-            $('#Wallker').show('slow');
-          }
-           if((index ==1 && direction == 'down' && nextIndex == 3) || (index == 2 && direction == 'down' && nextIndex == 3)){
-            $('#Maestro').show('slow');
-          }
-          if(index == 2 && (direction == 'up' || direction == 'down')){
-            $('#Wallker').hide('slow');
-          }
-          if(index == 3 && direction == 'up' && (nextIndex == 1 || nextIndex == 2)){
-            $('#Maestro').hide('slow');
-          }
-        }
-
     });
-
-    $('#arrow').click(function(event) {
-
-        $.fn.pagepiling.moveSectiondown();
-
-    });
-  });
 });
